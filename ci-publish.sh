c970fd1a9422c945c98dd0b5791d497f9f09c47e
Jichao Ouyang <oyanglulu@gmail.com>
2016-08-18 17:04:13 +0800

    mv to bash file

---
+++ ci-publish.sh
@@ -0,0 +1,6 @@
+echo "---" >> ~/.gem/credentials
+echo ":rubygems_api_key: $RUBYGEMS_API" >> ~/.gem/credentials
+chmod 600 ~/.gem/credentials
+VERSION=$(sed "s/v//" <<< $CIRCLE_TAG)
+gem build data.either/data.either.gemspec
+gem push data.either/data.either-$VERSION.gem
