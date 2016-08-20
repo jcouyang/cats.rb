767a4d5f00c78af728909fab42a2bb4c6fcee1bf
Jichao Ouyang <oyanglulu@gmail.com>
2016-08-20 16:47:26 +0800

    yard docs

--- ci-docs.sh
+++ ci-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages doc
-bundle exec yardoc lib/**.rb
+bundle exec yardoc lib/data.either.rb lib/control/*.rb
 git config --global user.name "circle ci"
 git config --global user.email "oyanglulu@gmail.com"
 cd doc
